2e9ed4cc80f2c58d894f04550073e1f8c42db533
daksh <dakshs0927@gmail.com>
2021-05-07 14:45:08 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var box1, pig1;
 var backgroundImg,platform;
 
 function preload() {
+   
     backgroundImg = loadImage("sprites/bg.png");
 }
 
@@ -17,7 +18,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -60,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
